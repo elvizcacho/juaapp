839d86b18bb9ad9dfd0e2b1029e4a2ca9d077348
Juan Vizcaino <elvizcacho@gmail.com>
2018-08-04 18:46:23 +0200

    Add header

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LogInService } from './shared/services/login.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(private logInService: LogInService) {
+
+  }
+
+  public isLoggedIn(): boolean {
+    return this.logInService.isLoggedIn();
+  }
 }
