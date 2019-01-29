c92a7d0c83133f71fe39f55d21a990b2b8f41fad
Juan Vizcaino <elvizcacho@gmail.com>
2019-01-29 16:00:05 +0100

    Re-structure freelancing-app

--- deployment/push-image-frontend.sh
+++ deployment/push-image-frontend.sh
@@ -2,7 +2,7 @@
 IMAGE_TAG=$(git branch | grep \* | cut -d ' ' -f2)-$(git log -1 --pretty=format:"%H" | tail -n 1)
 DOCKER_REGISTRY=elvizcacho
 IMAGE_NAME=juaapp-frontend
-cd frontend/ && docker image build . -t ${DOCKER_REGISTRY}/${IMAGE_NAME}
+cd freelancing/frontend/ && docker image build . -t ${DOCKER_REGISTRY}/${IMAGE_NAME}
 docker tag ${DOCKER_REGISTRY}/${IMAGE_NAME}:latest  ${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}
 docker push ${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}
 docker push ${DOCKER_REGISTRY}/${IMAGE_NAME}:latest
